4dcbb57e8a741401340d08b5535400643d3ab8f8
Prasad <sp.dobest@gmail.com>
2017-04-26 13:28:20 +0530

    prasad : initial commits

---
+++ app/src/androidTest/java/spdroid_adv/nstech/spdroid_adv/ExampleInstrumentedTest.java
@@ -0,0 +1,26 @@
+package spdroid_adv.nstech.spdroid_adv;
+
+import android.content.Context;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.*;
+
+/**
+ * Instrumentation test, which will execute on an Android device.
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+@RunWith(AndroidJUnit4.class)
+public class ExampleInstrumentedTest {
+    @Test
+    public void useAppContext() throws Exception {
+        // Context of the app under test.
+        Context appContext = InstrumentationRegistry.getTargetContext();
+
+        assertEquals("spdroid_adv.nstech.spdroid_adv", appContext.getPackageName());
+    }
+}
---
+++ app/src/main/java/spdroid_adv/nstech/spdroid_adv/MainActivity.java
@@ -0,0 +1,13 @@
+package spdroid_adv.nstech.spdroid_adv;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
