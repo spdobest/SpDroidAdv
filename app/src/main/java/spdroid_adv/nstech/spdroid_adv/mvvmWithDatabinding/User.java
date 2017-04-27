ab463e4d50a66f29e76f449d836ea265637ba609
Prasad <sp.dobest@gmail.com>
2017-04-27 16:56:48 +0530

    two databinding

--- app/src/main/java/spdroid_adv/nstech/spdroid_adv/mvvmWithDatabinding/User.java
+++ app/src/main/java/spdroid_adv/nstech/spdroid_adv/mvvmWithDatabinding/User.java
@@ -9,7 +9,25 @@ public class User extends BaseObservable {
  
     private String name;
     private String email;
- 
+    private boolean isActive;
+    private String address;
+
+    public boolean isActive() {
+        return isActive;
+    }
+
+    public void setActive(boolean active) {
+        isActive = active;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
     public String getName() {
         return name;
     }
